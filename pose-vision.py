6790cf556c225a97da5f1fde450821857b7257dd
Izet Delibasic <izetdelibasic55@gmail.com>
2024-06-22 19:19:59 +0200

    Updating color for fps counter and pose detection rectangle

--- pose-vision.py
+++ pose-vision.py
@@ -18,7 +18,11 @@ while True:
     fps = 1 / (cTime - pTime)
     pTime = cTime
     
-    cv2.putText(img, f'FPS: {int(fps)}', (20, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+    
+    if bboxInfo:
+        x, y, w, h = bboxInfo["bbox"]
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
     
     cv2.imshow("Pose Vision", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
